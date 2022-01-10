de2ffc5074a64be4692e599dcadd43ab6ba00a08
Alyssa Voccia <avoccia@hotmail.com>
2022-01-10 13:25:04 -0500

    Created the card component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback }) {
@@ -14,4 +16,14 @@ function FeedbackList({ feedback }) {
   )
 }
 
-export default FeedbackList
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  )
+}
+
+export default FeedbackList;
