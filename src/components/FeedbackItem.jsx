285ef98a684d0d581d884c79b826a37be5d22317
Alyssa Voccia <avoccia@hotmail.com>
2022-01-13 22:24:47 -0500

    Added feedback form and text state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -5,7 +5,7 @@ import Card from "./shared/Card";
 
 function FeedbackItem({item, handleDelete}) {
   return (
-    <Card reverse={true}>
+    <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
       <button className='close' onClick={() => handleDelete(item.id)}>
         <FaTimes color='purple' />
