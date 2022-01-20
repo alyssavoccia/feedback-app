76a55bd949f26eade08801a3acc348478425961b
Alyssa Voccia <avoccia@hotmail.com>
2022-01-20 17:45:49 -0500

    Moved the add and delete feedback functions to context.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,17 @@
 import { FaTimes } from 'react-icons/fa'
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 
+import FeedbackContext from '../context/FeedbackContext';
 import Card from "./shared/Card";
 
-function FeedbackItem({item, handleDelete}) {
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className='close' onClick={() => handleDelete(item.id)}>
+      <button className='close' onClick={() => deleteFeedback(item.id)}>
         <FaTimes color='purple' />
       </button>
       <div className="text-display">{item.text}</div>
